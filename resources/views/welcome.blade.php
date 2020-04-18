c68fdfe705b94db6f09854c4d2aa37de7c7fb3ac
Alexey <redspector@yandex.ru>
2020-04-18 12:12:45 +0300

    add demo page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Example API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a target="_blank" href="/api/adverts/create?title=test&description=test&price=123&photos[]=http://test&photos[]=http://test2">Создать объявление</a>
+                    <a target="_blank" href="/api/adverts/get-adverts?page=1&sort=price&sort_direction=desc">Список объявлений</a>
+                    <a target="_blank" href="/api/adverts/get-advert/1?fields[]=description&fields[]=all_photos">Объявление</a>
                 </div>
             </div>
         </div>
